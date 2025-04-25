63f046815226c17635a086ab8b00d0cd5639f4ec
MiddleBenchDev <middlebenchdev@gmail.com>
2025-04-25 11:54:29 +0530

    test changes

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin with the service account key
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
